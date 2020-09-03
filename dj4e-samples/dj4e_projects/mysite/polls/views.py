df37b8918dd1ba2a36741c558995c553105c2162
Nick <sshedl22@gmail.com>
2020-09-03 17:09:14 +0300

    Part 3 Django tutorial done

--- dj4e-samples/dj4e_projects/mysite/polls/views.py
+++ dj4e-samples/dj4e_projects/mysite/polls/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import Http404
+from .models import Question
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. eb184829 is the polls index.")
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
